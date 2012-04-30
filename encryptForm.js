03d7c674f970e43bb82b3be96004f842abbc7830
MGC <maxvgc@gmail.com>
2012-04-30 00:15:39 -0500

    thing are working. begin licensing and improve comments.

--- encryptForm.js
+++ encryptForm.js
@@ -1,29 +1,55 @@
-
-function encrypt(text){
-    return doEncrypt(keyid, keytyp, pubkey, text);
-}
-
-function send(cryptoText, server){
+function send(message, server, answer){
+    var mesg = document.getElementById(message); // store dom element
+    var ans = document.getElementById(answer); // store dom element
+    var cryptoText = mesg.value;
+    var challenge = ans.value;
     var xmlhttp = new XMLHttpRequest();
     
     xmlhttp.onreadystatechange=function()
     {
-	if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	{
-	    document.getElementById("response").innerHTML=xmlhttp.responseText;
+	if (xmlhttp.readyState==4){
+	    switch(xmlhttp.status){
+
+	    case 200:
+		document.getElementById("response").innerHTML
+		    ='message sent';
+		mesg.value = '';
+		ans.value = '';
+		reloadCaptcha();
+		break;
+
+	    case 403:
+		reloadCaptcha();
+		document.getElementById("response").innerHTML
+		    ='incorrect captcha';
+		reloadCaptcha();
+		ans.value = '';
+		break;
+
+	    default:
+		document.getElementById("response").innerHTML
+		    ='something went wrong, try again';
+	    }
 	}
     }
     
     xmlhttp.open("POST", server, true);
-    xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.send('message='+encodeURIComponent(cryptoText));
+    xmlhttp.setRequestHeader("Content-type",
+			     "application/x-www-form-urlencoded");
+    xmlhttp.send('message='+encodeURIComponent(cryptoText)
+		 +'&challenge='+encodeURIComponent(challenge));
     
 }
 
-function encryptAndSend(){
-    var plain = document.getElementById('text').value+'\r\n';
-    var cipher = encrypt(plain);
-    document.getElementById('text').value = cipher;
-    send(cipher, 'send.php');
-    return;
+
+function encrypt(text){
+    var plain = document.getElementById(text).value+'\r\n';
+    var cipher = doEncrypt(keyid, keytyp, pubkey, plain);
+    document.getElementById(text).value = cipher;
+}
+
+function reloadCaptcha()
+{
+    document.getElementById('siimage').src = 
+	'./secImg/securimage_show.php?sid=' + Math.random();
 }
--- send.php
+++ send.php
@@ -1,11 +1,30 @@
 <?php
-require('install/config.php');
+require_once('install/config.php');
 
-$result = mail($GLOBALS['recipient'], 'test email', $_POST['message']);
+if(validateCaptcha($_POST['challenge'])){
+  doSend();
 
-if($result == 1)
-echo 'sending to '.$GLOBALS['recipient'].' succeeded.';
-else
-echo 'sending failure';
+} else{
+  header("HTTP/1.0 403 Forbidden");
 
+}
+
+function validateCaptcha($captcha){           
+  require_once('secImg/securimage.php');
+  $securimage = new Securimage();
+
+  return $securimage->check($captcha);
+}
+
+
+function doSend(){
+  $result = mail($GLOBALS['recipient']
+  	    , 'test email', $_POST['message']);
+
+  if($result){
+    header("HTTP/1.0 200 OK");
+  } else{
+   header("HTTP/1.0 500 Internal Server Error");
+  }
+}
 ?>
