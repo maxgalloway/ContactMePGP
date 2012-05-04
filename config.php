09cf9ab9eeda46a6f3ea6463c84e2f185177730b
MaGiC <maxvgc@gmail.com>
2012-05-03 21:27:39 -0500

    got rid of some redundant files

--- config.php
+++
@@ -1,3 +0,0 @@
-<?php 
-$GLOBALS['recipient'] = 'maxvgc@gmail.com'; 
-?>
--- encryptForm.js
+++ encryptForm.js
@@ -8,7 +8,7 @@ Copyright (C) 2012 Max Galloway-Carson maxvgc@gmail.com
 This file is part of ContactMePGP.
     
 ContactMePGP is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
